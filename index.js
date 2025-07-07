f93ca2a46e4521b09afc41faa7060beabd21cdc9
Samarth2190 <samarthruia@gmail.com>
2025-07-07 11:31:01 +0530

    Added Error handling

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+const ExpressError = require('./ExpressError.js');
 const Chat=require('./models/chat.js');
+const { error } = require('console');
 async function main() { 
     await mongoose.connect('mongodb://localhost:27017/whatsapp');
 }
@@ -38,17 +40,16 @@ app.get('/',(req,res)=>{
 });
 
 
-app.get('/chat', async (req,res)=>{
+app.get('/chat', asyncWrap(async (req,res)=>{
     console.log("GET /chat route hit");
     let chats= await Chat.find();
-    // console.log(chats);
     res.render('index.ejs',{chats});
-})
+}));
 
-app.get('/chat/new', (req,res)=>{   
+app.get('/chat/new', asyncWrap(async(req,res)=>{   
     console.log("GET /chat/new route hit");
     res.render('new.ejs');
-}      );
+}));
 
 // app.post('/chat', async (req,res)=>{
 //     console.log("POST /chat route hit");
@@ -63,11 +64,10 @@ app.get('/chat/new', (req,res)=>{
 //     await chat.save().catch(err=>res.status(400).send(`<script>alert("${err.errors.msg.properties.message}"); window.location.href='/chat';</script>`));
 //     res.redirect('/chat');
 // });
-app.post('/chat', async (req, res) => {
+app.post('/chat', asyncWrap(async (req, res) => {
     console.log("POST /chat route hit");
     console.log(req.body);
 
-    try {
         let chat = new Chat({
             from: req.body.from,
             to: req.body.to,
@@ -78,22 +78,18 @@ app.post('/chat', async (req, res) => {
 
         await chat.save();
         res.redirect('/chat');
-    } catch (err) {
-        // console.error(err);
-        res.status(400).send(`<script>alert("${err.errors?.msg?.properties?.message || 'An error occurred'}"); window.location.href='/chat/new';</script>`);
-    }
-});
+ 
+}));
 
-app.get('/chat/:id/edit', async (req,res)=>{
+app.get('/chat/:id/edit', asyncWrap(async (req,res,next)=>{
     console.log("GET /chat/:id/edit route hit");
     let chat= await Chat.findById(req.params.id);
     console.log(chat);
     res.render('edit.ejs',{chat});
-}
-);
+}));
 
 
-app.put('/chat/:id', async (req,res)=>{
+app.put('/chat/:id',asyncWrap(async (req,res)=>{
     console.log("PUT /chat/:id route hit");
     console.log(req.body);
     let chat= await Chat.findByIdAndUpdate(req.params.id,{
@@ -104,10 +100,35 @@ app.put('/chat/:id', async (req,res)=>{
         runValidators: true,    
     });
     res.redirect('/chat');
-// }
-});
-app.delete('/chat/:id', async (req,res)=>{
+}));
+
+app.delete('/chat/:id', asyncWrap(async (req,res)=>{
     console.log("DELETE /chat/:id route hit");
     let chat= await Chat.findByIdAndDelete(req.params.id);
     res.redirect('/chat');
+}));
+function asyncWrap(fn) {
+    return function(req, res, next) {
+        fn(req, res, next).catch((err) =>next(err));    
+    };
+}
+const handleValidationError = (err) => {
+    console.log("THis is a validation error");
+    console.dir(err.message);
+    return err;
+}
+
+
+app.use((err, req, res, next) => {
+    console.log(err.name);
+    if(err.name==="ValidationError"){
+       err= handleValidationError(err);
+    }
+    next(err);
+});
+
+app.use((err,req,res,next)=>{
+    console.log(err.message);
+    let {status=500,message="Something went wrong"}=err;          
+    res.status(status).send(message);
 });
